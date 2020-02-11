653e945bbaa7dee39a0469436a44daeea334207d
taras <tarasulo@gmail.com>
2020-02-11 15:20:33 +0200

    fixed algorithm

--- serviceToXML/src/main/java/tkhal/service/stringToXMLController/CustomerFactory.java
+++ serviceToXML/src/main/java/tkhal/service/stringToXMLController/CustomerFactory.java
@@ -9,91 +9,88 @@ import java.util.LinkedList;
 
 public class CustomerFactory {
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerFactory.class);
+    private static final String BRACE = "\"";
 
     public static LinkedList<Customer> create(String message) {
         LinkedList<Customer> list = new LinkedList<>();
         String[] records = message.split("\n");
         for (String record : records) {
-            String symbol = record.substring(record.length()-1);
+            String symbol = record.substring(record.length() - 1);
             if (symbol.equals(",")) {
                 record += "?";
             }
-            String[] value = record.split(",");
-            if (value[18].equals("\"")) {
-                value[17] += ",\"";
-                value = ArrayUtils.removeElement(value, value[18]);
-            }
+            String[] value = validData(record);
             if (value.length < 23) {
                 LOGGER.error("Invalid message \n" + Arrays.toString(value));
             } else {
                 Customer customer = new Customer();
-                if (!(value[0].equals("?"))) customer.setAirline(value[0]);
-                if (!(value[1].equals("?"))) customer.setFlightNumber(value[1]);
-                if (!(value[2].equals("?"))) customer.setAcType(value[2]);
-                if (!(value[3].equals("?"))) customer.setValidTime(value[3]);
-                if (!(value[4].equals("?"))) customer.setCrossModelId(value[4]);
-                if (!(value[5].equals("?"))) customer.setTailNumber(value[5]);
-                if (!(value[6].equals("?"))) customer.setDepIcaoSiteId(value[6]);
-                if (!(value[7].equals("?"))) customer.setDepIcaoSiteId(value[7]);
-                if (!(value[8].equals("?") || value[8].length() == 0)) try {
+                if (empty(value[0])) customer.setAirline(value[0]);
+                if (empty(value[1])) customer.setFlightNumber(value[1]);
+                if (empty(value[2])) customer.setAcType(value[2]);
+                if (empty(value[3])) customer.setValidTime(value[3]);
+                if (empty(value[4])) customer.setCrossModelId(value[4]);
+                if (empty(value[5])) customer.setTailNumber(value[5]);
+                if (empty(value[6])) customer.setDepIcaoSiteId(value[6]);
+                if (empty(value[7])) customer.setDepIcaoSiteId(value[7]);
+                if (empty(value[8])) try {
                     customer.setCopyNum(Integer.parseInt(value[8]));
                 } catch (NumberFormatException e) {
                     LOGGER.error("error in element copyNum" + e);
                 }
-                if (!(value[9].equals("?") || value[9].length() == 0)) try {
+                if (empty(value[9])) try {
                     customer.setLat(Double.parseDouble(value[9]));
                 } catch (NumberFormatException e) {
                     LOGGER.error("Error in element lat" + e);
                 }
-                if (!(value[10].equals("?") || value[10].length() == 0)) try {
+                if (empty(value[10])) try {
                     customer.setLon(Double.parseDouble(value[10]));
                 } catch (NumberFormatException e) {
                     LOGGER.error("Error in element lon" + e);
                 }
-                if (!(value[11].equals("?"))) customer.setEventTime(value[11]);
-                if (!(value[12].equals("?") || value[12].length() == 0)) try {
+                if (empty(value[11])) customer.setEventTime(value[11]);
+                if (empty(value[12])) try {
                     customer.setFlightLevel(Integer.parseInt(value[12]));
                 } catch (NumberFormatException e) {
                     LOGGER.error("error in element flightLevel" + e);
                 }
-                if (!(value[13].equals("?") || value[13].length() == 0)) try {
+                if (empty(value[13])) try {
                     customer.setTemperature(Double.parseDouble(value[13]));
                 } catch (NumberFormatException e) {
                     LOGGER.error("Error in element temperature" + e);
                 }
-                if (!(value[14].equals("?") || value[14].length() == 0)) try {
+                if (empty(value[14])) try {
                     customer.setWindDirection(Integer.parseInt(value[14]));
                 } catch (NumberFormatException e) {
                     LOGGER.error("error in element windDirection" + e);
                 }
-                if (!(value[15].equals("?") || value[15].length() == 0)) try {
+                if (empty(value[15])) try {
                     customer.setWindSpeed(Integer.parseInt(value[15]));
                 } catch (NumberFormatException e) {
                     LOGGER.error("error in element windSpeed" + e);
                 }
-                if (!(value[16].equals("?"))) customer.setIcingPlaceholder(value[16]);
-                if (!(value[17].equals("?"))) customer.setEncOutput(value[17]);
-                if (!(value[18].equals("?") || value[18].length() == 0)) try {
+                if (empty(value[16])) customer.setIcingPlaceholder(value[16]);
+                if (empty(value[17])) customer.setEncOutput(value[17]);
+                if (empty(value[18])) try {
                     customer.setEdrMean(Double.parseDouble(value[18]));
                 } catch (NumberFormatException e) {
                     LOGGER.error("Error in element edrMean" + e);
                 }
-                if (!(value[19].equals("?") || value[19].length() == 0)) try {
+                if (empty(value[19])) try {
                     customer.setEdrPeak(Double.parseDouble(value[19]));
                 } catch (NumberFormatException e) {
                     LOGGER.error("Error in element edrPeak" + e);
                 }
-                if (!(value[20].equals("?") || value[20].length() == 0)) try {
+                if (empty(value[20])) try {
                     customer.setWvEncoding(Double.parseDouble(value[20]));
                 } catch (NumberFormatException e) {
                     LOGGER.error("error in element wvEncoding" + e);
                 }
-                if (!(value[21].equals("?") || value[21].length() == 0)) try {
+                if (empty(value[21])) try {
                     customer.setWvStatus(Double.parseDouble(value[21]));
                 } catch (NumberFormatException e) {
                     LOGGER.error("error in element wvStatus" + e);
                 }
-                if (!(value[22].equals("?") || value[22].length() == 0)) try {
+                if (empty(value[22])) try {
                     customer.setRmsva(Double.parseDouble(value[22]));
                 } catch (NumberFormatException e) {
                     LOGGER.error("Error in element rmsva" + e);
@@ -107,5 +104,26 @@ public class CustomerFactory {
             }
         }
         return list;
+    }
+
+    private static String[] validData(String record) {
+        String[] value = record.split(",");
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].length() > 0) {
+                if (value[i].substring(0, 1).equals(BRACE)) {
+                    String lastChar = value[i].substring(value[i].length() - 1);
+                    if (!lastChar.equals(BRACE)) {
+                        value[i] += "," + value[i + 1];
+                        value = ArrayUtils.removeElement(value, value[i + 1]);
+                    }
+                }
+            }
+        }
+        return value;
+    }
+
+    private static boolean empty(String rec) {
+        if (rec.equals("?") || rec.length() == 0) return false;
+        return true;
     }
 }
